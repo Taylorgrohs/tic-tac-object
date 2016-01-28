fb09075ec8c9b5216a6201a50ad8459307034570
Jay Whang and taylor grohs <taylorgrohs@gmail.com>
2016-01-28 14:56:09 -0800

    needs loop fix

--- js/scripts.js
+++ js/scripts.js
@@ -2,30 +2,70 @@ function Player (playerMark) {
   this.playerMark = playerMark;
 };
 
-Player.prototype.scoreBoard = function(postion) {
-  var holder = "";
-   holder += postion;
-   return holder;
-   console.log(holder);
+Player.prototype.scoreBoard = function(position) {
+
+  var holder = position;
+  return holder;
+  console.log(holder);
 };
 
 Player.prototype.winningCheck = function(score) {
   var winner = " ";
-  var winningCombo = ["abc",
-                     "def",
-                     "ghi",
-                     "adg",
-                     "beh",
-                     "cfi",
-                     "ceg",
-                     "aei"];
+  var winningCombo = {
+      row1: [1, 2, 4],
+      row2: [8, 16, 32],
+      row3: [64, 128, 256],
+      col1: [1, 8, 64],
+      col2: [2, 16, 128],
+      col3: [4, 32, 256],
+      cross1: [1, 16, 256],
+      cross2: [4, 16, 64]
+  };
 
-  winningCombo.forEach(function(index) {
-   if (score === index) {
-     winner = "player wins";
-   };
- });
+//debugger;
+if (score.length >= 3) {
+   var counterRow1 = 0;
+  // var counterRow2 = 0
+  // var counterRow3 = 0
+  // var counterCol1 = 0
+  // var counterCol2 = 0
+  // var counterCol3 = 0
+  // var counterCross1 = 0
+  // var counterCross2 = 0
+  debugger;
+  var counter =0;
+  for (var key in winningCombo) {
+    console.log(winningCombo[key][1]);
+    for (var j = 0; j <= winningCombo[key].length; j++) {
+      for (var i = 0; i <= score.length; i++) {
+        if (winningCombo[key][j] === score[i])
+        {
+          //console.log(winningCombo[key][1]);
+          counterRow1++;
+          if (counterRow1 === 3) {
+            winner = "player wins";
+          }
+        }
+      }
+    }
+ }
+}
  return winner;
+};
+
+
+
+
+
+var winning = {
+  row1: ["a", "b", "c"],
+  row2: ["d", "e", "f"],
+  row3: ["g", "h", "i"],
+  col1: ["a", "d", "g"],
+  col2: ["b", "e", "h"],
+  col3: ["c", "f", "i"],
+  cross1: ["c", "e", "g"],
+  cross2: ["a", "e"]
 };
 
 $(document).ready(function(){
@@ -36,35 +76,32 @@ $(document).ready(function(){
     var inputtedPlayer2 = new Player(player2);
     event.preventDefault();
     var count = 0;
-    var scorePlayer1 = "";
-    var scorePlayer2 = "";
+    var scorePlayer1 =[];
+    var scorePlayer2 =[];
     $("button.game").click(function(event){
+
       event.preventDefault();
+
       if (count % 2 === 0) {
         count++;
-
         $(this).text(inputtedPlayer1.playerMark);
-        var player1Postion = this.id;
-        scorePlayer1 += inputtedPlayer1.scoreBoard(player1Postion);
+        var player1Position = parseInt(this.id);
+        scorePlayer1.push(inputtedPlayer1.scoreBoard(player1Position));
         var winner = inputtedPlayer1.winningCheck(scorePlayer1);
-        console.log(winner);
         if (winner === "player wins") {
           alert("Player 1 wins!");
         };
-        console.log(count);
       }
       else {
         count++;
         $(this).text(inputtedPlayer2.playerMark);
-        var player2Postion = this.id;
-        scorePlayer2 += inputtedPlayer2.scoreBoard(player2Postion);
+        var player2Postion = parseInt(this.id);
+        scorePlayer2 = inputtedPlayer2.scoreBoard(player2Position);
         var winner = inputtedPlayer2.winningCheck(scorePlayer2);
         if (winner === "player wins") {
           alert("Player 2 wins!");
         };
       }
-    console.log(inputtedPlayer1);
-    console.log(inputtedPlayer2);
   });
 
   });
